30aac4a5824f1aadb543f94fb19e39b8a8d7467f
Jedrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2023-04-23 22:17:39 +0200

    Add spacy preprocessing script for kpwr

---
+++ scripts/kpwr_to_spacy_dataset.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+
+import argilla as rg
+from datasets import load_dataset
+from spacy.lang.pl import Polish
+from spacy.tokens import DocBin
+
+
+def make_docbin(dataset, nlp, span_key="sc"):
+    rg_dataset = rg.read_datasets(
+        dataset,
+        tags="ner",
+        task="TokenClassification",
+        metadata={"lemmas": "lemmas", "orth": "orth"},
+    )
+    docbin = rg_dataset.prepare_for_training(framework="spacy", lang=nlp)
+    docbin_wspans = DocBin()
+    for doc in docbin.get_docs(nlp.vocab):
+        doc.spans[span_key] = list(doc.ents)
+        docbin_wspans.add(doc)
+    return docbin_wspans
+
+
+def get_docbin_len(docbin, vocab):
+    return len(set(docbin.get_docs(vocab)))
+
+
+def non_empty_ner(record):
+    return True if record["ner"] else False
+
+
+def main():
+    OUT_DIR = Path("data") / "spacy_kpwr"
+    OUT_DIR.mkdir(exist_ok=True, parents=True)
+
+    dataset = load_dataset("clarin-pl/kpwr-ner").filter(function=non_empty_ner)
+
+    train_ds = dataset["train"]
+    val_ds, test_ds = dataset["test"].train_test_split(test_size=0.5).values()
+
+    nlp = Polish()
+    train_docbin = make_docbin(train_ds, nlp)
+    val_docbin = make_docbin(val_ds, nlp)
+    test_docbin = make_docbin(test_ds, nlp)
+
+    db_basename = "kpwr_spancat_{split}.spacy"
+    train_docbin.to_disk(OUT_DIR / db_basename.format(split="train"))
+    val_docbin.to_disk(OUT_DIR / db_basename.format(split="val"))
+    test_docbin.to_disk(OUT_DIR / db_basename.format(split="test"))
+
+    train_len = get_docbin_len(train_docbin, nlp.vocab)
+    val_len = get_docbin_len(val_docbin, nlp.vocab)
+    test_len = get_docbin_len(test_docbin, nlp.vocab)
+    total_docs = train_len + val_len + test_len
+
+    info = (
+        "KPWR dataset in Spacy's DocBin format.\n"
+        "Source: https://huggingface.co/datasets/clarin-pl/kpwr-ner \n"
+        "Validation and test sets represent dataset's 'test' split (50-50)\n"
+        "Records with empty 'ner' field were omitted.\n\n"
+        f"Train docs: {train_len} ({train_len / total_docs:.2%})\n"
+        f"Validation docs: {val_len} ({val_len / total_docs:.2%})\n"
+        f"Test docs: {test_len} ({test_len / total_docs:.2%})\n"
+    )
+    with open(OUT_DIR / "info.txt", "w") as f:
+        f.write(info)
+    print(info)
+
+
+if __name__ == "__main__":
+    main()
